cdba85301e9b8616cdfce40e4b3cd7fa3fda2d68
TAK <gimg26746@naver.com>
2024-11-08 16:53:19 +0900

    커밋3

---
+++ app/src/main/java/com/example/myapplication_123/TitleActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication_123;
+
+import android.content.Intent; // Intent 클래스 임포트
+import android.os.Bundle; // Bundle 클래스 임포트
+import android.view.View; // View 클래스 임포트
+import android.widget.Button; // Button 클래스 임포트
+import androidx.appcompat.app.AppCompatActivity; // AppCompatActivity 클래스 임포트
+
+public class TitleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); // 부모 클래스의 onCreate 메서드 호출
+        setContentView(R.layout.title); // title.xml 레이아웃 설정
+
+        // 버튼을 찾아서 참조를 가져옴
+        Button imageButton = (Button) findViewById(R.id.button2);
+
+        // 버튼 클릭 리스너 설정
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // MainActivity로 이동하기 위한 Intent 생성
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent); // 새로운 Activity 시작
+            }
+        });
+    }
+}
